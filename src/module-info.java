7bc729c8266bf2b1245b1bdae890bfbe89d6a83a
dlrzz <arkad.gawry@gmail.com>
2019-10-09 10:17:23 +0200

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module MTG.BigProjectFX {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.swing;
+    requires org.jsoup;
+    requires org.json;
+    requires java.desktop;
+    requires imgscalr.lib;
+    requires org.junit.jupiter;
+    requires org.junit.jupiter.api;
+
+    opens com.dlrzz.MTGGame;
+}
